42f8ac53dfd489a0c8fd52ce00a5c84ca37734f2
Balunywa <Lukman@taacs.cloud>
2023-03-18 21:25:57 -0400

    updates

--- routes.py
+++ routes.py
@@ -13,13 +13,14 @@ import tempfile
 from io import BytesIO
 from flask import render_template, Response
 from docx import Document
+import requests
 
 
 
 
 
 # Replace 'your_app' with the name of your Flask application
-openai.api_key = "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"
+openai.api_key = "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"
 # Routes and views
 
 @app.before_first_request
@@ -121,48 +122,36 @@ def select_template(idea_id):
 
     return render_template('select_template.html', templates=templates, idea_id=idea_id)
 
-""""
-def generate_business_case(idea_id):
-    # Get the idea object from the database
-    idea = Idea.query.get(idea_id)
 
-    # Describe the desired structure in the prompt for the GPT API
-    #-1-1-prompt = f"Generate a business case for adopting the proposed solution '{idea.title}', which addresses the problem '{idea.problem}' and targets the market '{idea.target_market}'. Consider the competition '{idea.competition}' and the key differentiators '{idea.key_differentiators}'. The business case should follow the structure provided and include actual data and metrics to support the case:\n\nExecutive Summary:\nProblem Statement:\nObjectives:\n1. Objective 1\n2. Objective 2\n3. Objective 3\n4. Objective 4\n5. Objective 5\n\nProposed Solution:\n1. Benefit 1\n2. Benefit 2\n3. Benefit 3\n4. Benefit 4\n5. Benefit 5\n6. Benefit 6\n\nCost Analysis:\n1. Initial setup and configuration costs\n2. Ongoing infrastructure and service costs\n3. Potential cost savings through optimization and efficiency gains\n4. Costs associated with training and knowledge transfer\n\nROI and Benefits Realization:\n1. KPI 1\n2. KPI 2\n3. KPI 3\n4. KPI 4\n\nNext Steps:\n1. Conduct a detailed cost-benefit analysis\n2. Develop a phased implementation plan\n3. Identify and prioritize applications and services to migrate\n4. Provide training and knowledge transfer\n5. Establish a governance model and assign roles and responsibilities\n\nConclusion:"
-    prompt = f"Generate a business case for adopting the proposed solution '{idea.title}', which addresses the problem '{idea.problem}' and targets the market '{idea.target_market}'. Consider the competition '{idea.competition}' and the key differentiators '{idea.key_differentiators}'. The business case should follow the structure provided and include actual data and metrics to support the case:\n\nExecutive Summary:\nProblem Statement:\nObjectives:\n1. Objective 1\n2. Objective 2\n3. Objective 3\n4. Objective 4\n5. Objective 5\n\nProposed Solution:\n1. Benefit 1\n2. Benefit 2\n3. Benefit 3\n4. Benefit 4\n5. Benefit 5\n6. Benefit 6\n\nCost Analysis:\n1. Initial setup and configuration costs\n2. Ongoing infrastructure and service costs\n3. Potential cost savings through optimization and efficiency gains\n4. Costs associated with training and knowledge transfer\n\nROI and Benefits Realization:\n1. KPI 1\n2. KPI 2\n3. KPI 3\n4. KPI 4\n\nNext Steps:\n1. Conduct a detailed cost-benefit analysis\n2. Develop a phased implementation plan\n3. Identify and prioritize applications and services to migrate\n4. Provide training and knowledge transfer\n5. Establish a governance model and assign roles and responsibilities\n\nConclusion:"
-    # Make the GPT API request
-    response = openai.Completion.create(
-        engine="davinci-codex",
-        prompt=prompt,
-        max_tokens=500,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-
-    # Extract the generated business case from the API response
-    generated_business_case = response.choices[0].text.strip()
-
-    # Return the generated business case
-    return generated_business_case
-"""
 
 def generate_business_case(idea_id):
-    # Get the idea object from the database
     idea = Idea.query.get(idea_id)
 
-    prompt = f"Generate a business case for adopting the proposed solution '{idea.title}', which addresses the problem '{idea.problem}' and targets the market '{idea.target_market}'. Please include the following sections:\n\n1. Problem statement and proposed solution\n2. Market analysis and target audience\n3. Financial projections, including cost and revenue estimates\n4. Project timeline and milestones\n\n"
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": f"Generate a business case for adopting the proposed solution '{idea.title}', which addresses the problem '{idea.problem}' and targets the market '{idea.target_market}'. Consider the competition '{idea.competition}' and the key differentiators '{idea.key_differentiators}'. The business case should follow the structure provided and include actual data and metrics to support the case."}
+    ]
 
-    response = openai.Completion.create(
-        engine="davinci-codex",
-        prompt=prompt,
-        max_tokens=500,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
+    url = "https://api.openai.com/v1/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer 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"
+    }
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": messages,
+        "max_tokens": 500,
+        "n": 1,
+        "stop": None,
+        "temperature": 0.3
+    }
 
-    generated_business_case = response.choices[0].text.strip()
+    response = requests.post(url, headers=headers, json=data)
+    response_json = response.json()
+    generated_business_case = response_json['choices'][0]['message']['content'].strip()
+
     return generated_business_case
+
 
 
 @app.route('/generate_business_case/<int:idea_id>', methods=['GET'])
@@ -187,79 +176,3 @@ def download_business_case(idea_id):
     in_memory_file.close()
 
     return response
-
-
-
-
-
-"""
-@app.route('/generate_business_case/<int:idea_id>', methods=['GET'])
-@login_required
-def download_business_case(idea_id):
-    # Generate the business case document
-    business_case = generate_business_case(idea_id)
-
-    # Create an in-memory file-like object to write the generated document to
-    in_memory_file = BytesIO()
-
-    # Convert the generated document to a Word document using python-docx
-    document = python-docx.Document()
-    document.add_paragraph(business_case)
-    document.save(in_memory_file)
-
-    # Return the file as an attachment with the specified filename
-    response = Response(in_memory_file.getvalue(), content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
-    response.headers['Content-Disposition'] = 'attachment; filename=business_case.docx'
-
-    # Close the in-memory file
-    in_memory_file.close()
-
-    return response
-
-"""
-
-"""
-def generate_business_case(idea_id):
-    # Get the idea object from the database
-    idea = Idea.query.get(idea_id)
-
-    # Describe the desired structure in the prompt for the GPT API
-    prompt = f"Generate a business case for adopting the proposed solution '{idea.title}', which addresses the problem '{idea.problem}' and targets the market '{idea.target_market}'. Consider the competition '{idea.competition}' and the key differentiators '{idea.key_differentiators}'. The business case should follow the structure provided and include actual data and metrics to support the case:\n\nExecutive Summary:\nProblem Statement:\nObjectives:\n1. Objective 1\n2. Objective 2\n3. Objective 3\n4. Objective 4\n5. Objective 5\n\nProposed Solution:\n1. Benefit 1\n2. Benefit 2\n3. Benefit 3\n4. Benefit 4\n5. Benefit 5\n6. Benefit 6\n\nCost Analysis:\n1. Initial setup and configuration costs\n2. Ongoing infrastructure and service costs\n3. Potential cost savings through optimization and efficiency gains\n4. Costs associated with training and knowledge transfer\n\nROI and Benefits Realization:\n1. KPI 1\n2. KPI 2\n3. KPI 3\n4. KPI 4\n\nNext Steps:\n1. Conduct a detailed cost-benefit analysis\n2. Develop a phased implementation plan\n3. Identify and prioritize applications and services to migrate\n4. Provide training and knowledge transfer\n5. Establish a governance model and assign roles and responsibilities\n\nConclusion:"
-
-    # Make the GPT API request
-    response = openai.Completion.create(
-        engine="davinci-codex",
-        prompt=prompt,
-        max_tokens=500,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-
-    # Extract the generated business case from the API response
-    generated_business_case = response.choices[0].text.strip()
-
-    # Return the generated business case
-    return generated_business_case
-
-
-@app.route('/generate_business_case/<int:idea_id>', methods=['GET'])
-@login_required
-def download_business_case(idea_id):
-    # Generate the business case document
-    business_case = generate_business_case(idea_id)
-
-    # Create an in-memory file-like object to write the generated document to
-    in_memory_file = BytesIO()
-    in_memory_file.write(business_case.encode('utf-8'))
-    in_memory_file.seek(0)
-
-    # Return the file as an attachment with the specified filename
-    response = Response(in_memory_file.read(), content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
-    response.headers['Content-Disposition'] = 'attachment; filename=business_case.docx'
-
-    # Close the in-memory file
-    in_memory_file.close()
-
-    return response
-"""
